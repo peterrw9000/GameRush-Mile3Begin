e440ca3c986b6bfe4f795afb9979b7fd158b4018
michael504-B <91339358+michael504-B@users.noreply.github.com>
2022-04-12 21:06:14 -0500

    all animation are imported and work

---
+++ GameProjectDev-main-main/Game_Rush/Assets/WeakSpot.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class WeakSpot : MonoBehaviour
+{
+    [SerializeField]
+    int weakSpotDmg = 15; 
+
+    // this will be whatever health scripts the enemies are sharing for testing it was tankhealth
+    public EnemyHealth enemyHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // just replace tankHealth with the enemies script that they are sharing
+    public void DamageToEnemy(int dmg)
+    {
+        dmg = weakSpotDmg;
+        //tankHealth = gameObject.GetComponentInParent<TankHealth>();
+        enemyHealth.TakeDamage(dmg);
+        Destroy(gameObject);
+     
+    }
+
+
+}
