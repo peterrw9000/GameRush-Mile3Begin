b40ca4357f79ca665627e59f574552a4e0fca7be
peterrw9000 <peterrw9001@yahoo.com>
2022-04-14 20:15:17 -0500

    Boss Microgame Attacks 2

--- GameProjectDev-main-main/Game_Rush/Assets/MicroGames/BossMicrogame/Scripts/BossMicrogameAttack.cs
+++ GameProjectDev-main-main/Game_Rush/Assets/MicroGames/BossMicrogame/Scripts/BossMicrogameAttack.cs
@@ -10,12 +10,29 @@ public class BossMicrogameAttack : MonoBehaviour
     float shieldAttackTimer = 0;
 
     bool wallAttackFinished = false;
+    bool wallAttackEnding = false;
     bool shieldAttackFinished = true;
+    bool shieldAttackStarting = false;
+    bool shieldAttackEnding = false;
+
+    int wallAttacking = 1;
 
     public float speed;
     public float wallSpeed;
 
-    float xdirection;
+    public float xdirection;
+    public float alphaTime;
+
+    public Transform deathWall1;
+    public Transform deathWall2;
+
+    public Collider shield1;
+    public Collider shield2;
+    public Material shieldAlpha;
+
+    public Transform shieldtest;
+
+    public Transform camPosition;
 
     public GameObject warning;
     public Transform LazerSpawn;
@@ -41,22 +58,90 @@ public class BossMicrogameAttack : MonoBehaviour
                 attackTimer = 0;
             }
         }
-        if (shieldAttackFinished == true)
+        if (camPosition.position.y >= 55)
         {
-            wallAttackTimer += Time.deltaTime;
-            if (wallAttackTimer >= 10)
+            if (shieldAttackFinished == true)
             {
-
-                //if ()
-                //wallAttackFinished = true;
+                wallAttackTimer += Time.deltaTime;
+                if (wallAttackTimer >= 10)
+                {
+                    wallAttackFinished = false;
+                    if (wallAttacking == 1)
+                    {
+                        deathWall1.transform.Translate(new Vector3(xdirection * wallSpeed, 0, 0));
+                        if (deathWall1.position.x >= -11)
+                        {
+                            wallAttackEnding = true;
+                            xdirection *= -1;
+                        }
+                        if (deathWall1.position.x == -40 && wallAttackEnding == true)
+                        {
+                            wallAttacking = 2;
+                            wallAttackFinished = true;
+                            shieldAttackFinished = false;
+                            wallAttackTimer = 0;
+                        }
+                    }
+                    else
+                    {
+                        if (deathWall2.position.x <= 11)
+                        {
+                            wallAttackEnding = true;
+                            xdirection *= -1;
+                            deathWall2.transform.Translate(new Vector3(xdirection * wallSpeed, 0, 0));
+                        }
+                        else
+                        {
+                            deathWall2.transform.Translate(new Vector3(xdirection * wallSpeed, 0, 0));
+                        }
+                        if (deathWall2.position.x == 40 && wallAttackEnding == true)
+                        {
+                            wallAttacking = 1;
+                            wallAttackFinished = true;
+                            shieldAttackFinished = false;
+                            wallAttackTimer = 0;
+                        }
+                    }
+                }
             }
-        }
-        if (wallAttackFinished == true)
-        {
-            shieldAttackTimer += Time.deltaTime;
-            if (shieldAttackTimer >= 5)
+            if (wallAttackFinished == true)
             {
-                shieldAttackFinished = false;
+                shieldAttackTimer += Time.deltaTime;
+                if (shieldAttackTimer >= 2)
+                {
+                    shieldAttackFinished = false;
+                    shieldAttackStarting = true;
+                    Color shieldColor = shieldAlpha.color;
+                    while (shieldAlpha.color.a < 1 && shieldAttackStarting == true)
+                    {
+                        shieldColor.a += alphaTime * Time.deltaTime;
+                        shieldAlpha.color = shieldColor;
+                        if (shieldAlpha.color.a == 1)
+                        {
+                            shieldAttackStarting = false;
+                            shieldAttackEnding = true;
+                        }
+                    }
+                    while (shieldAlpha.color.a > 0 && shieldAttackEnding == true)
+                    {
+                        shieldColor.a -= alphaTime * Time.deltaTime;
+                        shieldAlpha.color = shieldColor;
+                        if (shieldAlpha.color.a == 0)
+                        {
+                            shieldAttackTimer = 0;
+                            wallAttackFinished = false;
+                            shieldAttackFinished = true;
+                        }
+                    }
+                    if (shieldAlpha.color.a > 0)
+                    {
+                        shield1.enabled = true;
+                    }
+                    else
+                    {
+                        shield1.enabled = false;
+                    }    
+                }
             }
         }
     }
