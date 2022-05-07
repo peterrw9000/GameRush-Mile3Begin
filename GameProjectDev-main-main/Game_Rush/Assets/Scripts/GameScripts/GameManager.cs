434abb5fe811d89501f0519b9f08ac4953cc52ae
gageswan <70035004+gageswan@users.noreply.github.com>
2022-05-07 18:03:02 -0500

    Nerf Soldier, Remove Debug Break

--- GameProjectDev-main-main/Game_Rush/Assets/Scripts/GameScripts/GameManager.cs
+++ GameProjectDev-main-main/Game_Rush/Assets/Scripts/GameScripts/GameManager.cs
@@ -31,9 +31,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("z")) {
+/*        if (Input.GetKeyDown("z")) {
             Debug.Break();
-        }
+        }*/
 
         Scene scene = SceneManager.GetActiveScene();
 
